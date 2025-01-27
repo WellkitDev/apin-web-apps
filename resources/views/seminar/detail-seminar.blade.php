c9967e64312899ae6fd527a51121e439631c00ba
Wellkit <rahmatpurnomo808@gmail.com>
2025-01-27 16:25:26 +0700

    Enhance seminar detail page with new styles and structured layout

--- resources/views/seminar/detail-seminar.blade.php
+++ resources/views/seminar/detail-seminar.blade.php
@@ -21,6 +21,7 @@
         </div>
     </div>
     <!-- end of wpo-breadcumb-section-->
+
     <!-- start of wpo-project-single-section -->
     <section class="wpo-project-single-section section-padding">
         <div class="container">
@@ -28,39 +29,86 @@
                 <div class="col-lg-10 offset-lg-1">
                     <div class="wpo-project-single-wrap">
                         <div class="wpo-project-single-content">
-                            <img src="https://wpocean.com/html/tf/consultar-live/assets/images/project-single/1.jpg"
-                                alt="">
                             <div class="wpo-project-single-content-des">
                                 <div class="wpo-project-single-content-des-wrap">
-                                    <div class="wpo-project-single-content-des-left seminar-single-info">
-                                        <h2>8 Mistakes Founders Make When Starting a
-                                            Business.</h2>
-                                        <p>I must explain to you how all this mistaken idea of denouncing pleasure and
-                                            praising pain was born and I will give you a complete account of the system, and
-                                            expound the actual teachings of the great explorer of the truth, the
-                                            master-builder of human happiness. No one rejects, dislikes, or avoids pleasure
-                                            itself, because it is pleasure, but because those who do not know how to pursue
-                                            pleasure rationally encounter consequences that are extremely painful. </p>
-                                        <p>Nor again is there anyone who loves or pursues or desires to obtain pain of
-                                            itself, because it is pain, but because occasionally circumstances occur in
-                                            which toil and pain can procure him some great pleasure. To take a trivial
-                                            example, which of us ever undertakes laborious physical exercise.</p>
+                                    <div class="wpo-project-single-content-des-left seminar-single-info mb-3">
+                                        <img src="https://simoster.org/imagessempro/Sempro_SP17012025011716.png"
+                                            alt="">
+                                        <div class="col mt-3 seminar-btn-grup text-center">
+                                            <a class="theme-btn-s2 success mb-2" href="#">Cp 082279814793</a>
+                                            <a class="theme-btn-s2 primary" href="#">Daftar</a>
+                                        </div>
                                     </div>
                                     <div class="wpo-project-single-content-des-right seminar-single-detail">
-                                        <ul>
-                                            <li>Judul :<span>8 Mistakes Founders Make When Starting a
-                                                    Business.</span></li>
-                                            <li>Penyelenggara :<span>ARIPAFI - ASOSIASI RISET ILMU PENDIDIKAN AGAMA DAN
-                                                    FILSAFAT INDONESIA (ARIPAFI)</span></li>
-                                            <li>Jenis :<span>SEMINAR</span></li>
-                                            <li>Tanggal : <span>17 Jan 2025 s.d 28 Feb 2025</span></li>
-                                            <li>Lokasi : <span>Zoom</span></li>
-                                            <li>Biaya :<span>100.000</span></li>
-                                            <li>Rek. Pembayaran :<span>-BANK *BRI* 224201000464569 an Lembaga Pengembangan
-                                                    Kinerja Dosen</span></li>
-                                            <li>URL Grup WA :<span><a href="#">Gabung Grup</a></span></li>
-                                            <li>CP :<span><a href="">082279814793</a></span></li>
-                                        </ul>
+                                        <table class="table">
+                                            <tbody>
+                                                <tr>
+                                                    <td>Judul</td>
+                                                    <td>8 Mistakes Founders Make When Starting a
+                                                        Business.</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Penyelenggara</td>
+                                                    <td>ARIPAFI - ASOSIASI RISET ILMU PENDIDIKAN AGAMA DAN
+                                                        FILSAFAT INDONESIA (ARIPAFI)</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Jenis</td>
+                                                    <td>SEMINAR</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Tanggal</td>
+                                                    <td>17 Jan 2025 s.d 28 Feb 2025</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Lokasi</td>
+                                                    <td>Zoom</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Biaya</td>
+                                                    <td>100.000</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Rek. Pembayaran</td>
+                                                    <td>-BANK *BRI* 224201000464569 an Lembaga Pengembangan
+                                                        Kinerja Dosen</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>URL Grup WA</td>
+                                                    <td><a href="#">Gabung Grup</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>CP</td>
+                                                    <td><a href="">082279814793</a></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Deskripsi</td>
+                                                    <td>
+                                                        <p>I must explain to you how all this mistaken idea of denouncing
+                                                            pleasure and
+                                                            praising pain was born and I will give you a complete account of
+                                                            the system, and
+                                                            expound the actual teachings of the great explorer of the truth,
+                                                            the
+                                                            master-builder of human happiness. No one rejects, dislikes, or
+                                                            avoids pleasure
+                                                            itself, because it is pleasure, but because those who do not
+                                                            know how to pursue
+                                                            pleasure rationally encounter consequences that are extremely
+                                                            painful. </p>
+                                                        <p>Nor again is there anyone who loves or pursues or desires to
+                                                            obtain pain of
+                                                            itself, because it is pain, but because occasionally
+                                                            circumstances occur in
+                                                            which toil and pain can procure him some great pleasure. To take
+                                                            a trivial
+                                                            example, which of us ever undertakes laborious physical
+                                                            exercise.</p>
+                                                    </td>
+                                                </tr>
+
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
                             </div>
