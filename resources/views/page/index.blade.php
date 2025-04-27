92216dfe525e738c458399c83f68e914daac15d3
Wellkit <rahmatpurnomo808@gmail.com>
2025-04-27 13:03:07 +0700

    Refactor service and about sections: update layout and image handling for improved presentation and user experience

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -20,51 +20,92 @@
     <!-- end of wpo-breadcumb-section-->
 
     <!-- start of wpo-about-section -->
-    <section class="wpo-service-single-section section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12">
-                    <div class="wpo-service-single-wrap">
-                        <div class="wpo-service-single-content">
-                            <img src="{{ $page->image }}"
-                                alt="">
-                            <div class="wpo-service-single-content-des">
-                                <div class="">
-                                    {!! $page->description !!}
-                                </div>
+    @if ($page->pageItems->isEmpty())
+        <section class="wpo-about-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 col-12">
+                        <div class="wpo-about-wrap">
+                            <div class="wpo-about-img">
+                                <img src="{{ $page->image }}" alt="">
+                                <div class="wpo-ab-shape-1"><img
+                                        src="https://wpocean.com/html/tf/consultar-live/assets/images/ab-shape-1.png"
+                                        alt=""></div>
+                                <div class="wpo-ab-shape-2"><img
+                                        src="https://wpocean.com/html/tf/consultar-live/assets/images/ab-shape-2.png"
+                                        alt=""></div>
                             </div>
                         </div>
-                        @if ($page->pageItems->isNotEmpty())
-                        <div class="wpo-solutions-section">
-                            {{-- <h2>Our Solutions</h2> --}}
-                            <div class="row">
-                                @foreach ($page->pageItems as $item)
-                                <div class="col-lg-4 col-md-6 col-12">
-                                    <div class="wpo-solutions-item">
-                                        <div class="wpo-solutions-icon">
-                                            <div class="icon">
-                                                <img src="{{ $item->image ?? 'https://wpocean.com/html/tf/consultar-live/assets/images/icon/document.svg' }}"
-                                                         alt="{{ $item->title }}">
-                                            </div>
-                                        </div>
-                                        <div class="wpo-solutions-text">
-                                            <h2><a href="{{ url($page->slug . '/' . $item->slug) }}">{{ $item->title }}</a></h2>
-                                            {{-- <p>{{ $item->description ?? 'The lower-numbered purposes are better understood and practiced' }}</p> --}}
-                                            <p>{{ Str::limit(strip_tags($item->description), 120) ?? 'The lower-numbered purposes are better understood and practiced' }}</p>
-
-                                        </div>
-                                    </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-12">
+                        <div class="wpo-about-text">
+                            <div class="wpo-about-icon">
+                                <div class="icon">
+                                    <img src="https://wpocean.com/html/tf/consultar-live/assets/images/icon/badge.svg"
+                                        alt="">
                                 </div>
-                                @endforeach
+                            </div>
+                            <div class="wpo-about-icon-content">
+                                {!! $page->description !!}
                             </div>
                         </div>
-                        @endif
                     </div>
                 </div>
-                
             </div>
-        </div>
-    </section>
+        </section>
+    @else
+        <section class="wpo-service-single-section section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="wpo-service-single-wrap">
+                            <div class="wpo-service-single-content">
+                                <img src="{{ $page->image }}" alt="">
+                                <div class="wpo-service-single-content-des">
+                                    <div class="">
+                                        {!! $page->description !!}
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="wpo-solutions-section">
+                                {{-- <h2>Our Solutions</h2> --}}
+                                <div class="row">
+                                    @foreach ($page->pageItems as $item)
+                                        <div class="col-lg-4 col-md-6 col-12">
+                                            <div class="wpo-solutions-item">
+                                                <div class="wpo-solutions-icon">
+                                                    <div class="icon">
+                                                        <img src="{{ $item->image ?? 'https://wpocean.com/html/tf/consultar-live/assets/images/icon/document.svg' }}"
+                                                            alt="{{ $item->title }}">
+                                                    </div>
+                                                </div>
+                                                <div class="wpo-solutions-text">
+                                                    <h2><a
+                                                            href="{{ url($page->slug . '/' . $item->slug) }}">{{ $item->title }}</a>
+                                                    </h2>
+                                                    {{-- <p>{{ $item->description ?? 'The lower-numbered purposes are better understood and practiced' }}</p> --}}
+                                                    <p>{{ Str::limit(strip_tags($item->description), 120) ?? 'The lower-numbered purposes are better understood and practiced' }}
+                                                    </p>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    @endif
+
+    {{-- @if ($page->pageItems->isNotEmpty())
+
+    @endif --}}
     <!-- end of wpo-about-section -->
-    
+
 @endsection
--- resources/views/pages/show-sub.blade.php
+++ resources/views/pages/show-sub.blade.php
@@ -20,14 +20,45 @@
     <!-- end of wpo-breadcumb-section-->
 
     <!-- start of wpo-about-section -->
-    <section class="wpo-service-single-section section-padding">
+    <section class="wpo-about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-12">
+                    <div class="wpo-about-wrap">
+                        <div class="wpo-about-img">
+                            <img src="{{ $subPage->image }}" alt="">
+                            <div class="wpo-ab-shape-1"><img
+                                    src="https://wpocean.com/html/tf/consultar-live/assets/images/ab-shape-1.png"
+                                    alt=""></div>
+                            <div class="wpo-ab-shape-2"><img
+                                    src="https://wpocean.com/html/tf/consultar-live/assets/images/ab-shape-2.png"
+                                    alt=""></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-12">
+                    <div class="wpo-about-text">
+                        <div class="wpo-about-icon">
+                            <div class="icon">
+                                <img src="https://wpocean.com/html/tf/consultar-live/assets/images/icon/badge.svg"
+                                    alt="">
+                            </div>
+                        </div>
+                        <div class="wpo-about-icon-content">
+                            {!! $subPage->description !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- <section class="wpo-service-single-section section-padding">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12">
                     <div class="wpo-service-single-wrap">
                         <div class="wpo-service-single-content">
-                            <img src="{{ $subPage->image }}"
-                                alt="">
+                            <img src="{{ $subPage->image }}" alt="">
                             <div class="wpo-service-single-content-des">
                                 <div class="">
                                     {!! $subPage->description !!}
@@ -36,10 +67,9 @@
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </div>
-    </section>
+    </section> --}}
     <!-- end of wpo-about-section -->
-    
 @endsection
