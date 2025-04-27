98644c2c0d83a7f668aad770029b3e60f0da7645
Wellkit <rahmatpurnomo808@gmail.com>
2025-04-27 14:10:33 +0700

    Implement index method in AdminCategoryController to fetch and display categories

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -8,4 +8,12 @@ use Illuminate\Http\Request;
 class AdminCategoryController extends Controller
 {
     //
+    public function index()
+    {
+        // Fetch all categories from the database
+
+        // Return the view with the categories data
+        // return view('admin.categories.index', compact('categories'));
+        return view('backend.blog.categories.index');
+    }
 }
