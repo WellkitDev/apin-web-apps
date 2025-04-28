1a2c0edad502ecf00300d0c4664ccd6a908c3ba2
Wellkit <rahmatpurnomo808@gmail.com>
2025-04-28 19:23:58 +0700

    Implement category creation functionality in AdminCategoryController

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Illuminate\Support\Str;
 
 class AdminCategoryController extends Controller
 {
@@ -22,4 +24,26 @@ class AdminCategoryController extends Controller
     {
         return view('backend.blog.categories.create');
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:50',
+            'is_active' => 'required',
+            'order' => 'required',
+        ]);
+
+        $category = new Category();
+
+        $category->category_name = $request->title;
+        $category->category_slug = Str::slug($request->title);
+        $category->category_order = $request->order;
+        $category->show_on_menu = $request->is_active;
+
+        $category->save();
+
+        return redirect()->back()->with('success', 'Category created successfully');
+    }
+
+
 }
