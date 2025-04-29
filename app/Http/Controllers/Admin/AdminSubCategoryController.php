b4a61ebe9ff3ee5de25482027a9106c071ba3fdd
Wellkit <rahmatpurnomo808@gmail.com>
2025-04-29 21:56:11 +0700

    Add create method to AdminSubCategoryController for sub-category creation

--- app/Http/Controllers/Admin/AdminSubCategoryController.php
+++ app/Http/Controllers/Admin/AdminSubCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class AdminSubCategoryController extends Controller
 {
@@ -11,5 +12,13 @@ class AdminSubCategoryController extends Controller
     public function index()
     {
         return view('backend.blog.sub-category.index');
+    }
+
+    //
+    public function create()
+    {
+        $categories = Category::orderBy('category_order', 'asc')->get();
+
+        return view('backend.blog.sub-category.create', compact('categories'));
     }
 }
