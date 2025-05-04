dac3cc6235f6876651a54ed76a7260ee26491f85
Wellkit <rahmatpurnomo808@gmail.com>
2025-05-05 00:32:11 +0700

    Refactor article display logic to dynamically render article data and improve content presentation

--- resources/views/blog/artikel.blade.php
+++ resources/views/blog/artikel.blade.php
@@ -25,44 +25,29 @@
             <div class="row">
                 <div class="col col-lg-8">
                     <div class="wpo-blog-content">
-                        <div class="post format-standard-image">
-                            <div class="entry-media">
-                                <img src="https://wpocean.com/html/tf/consultar-live/assets/images/blog/img-4.jpg" alt>
+                        @foreach ($article_data as $item)
+                            <div class="post format-standard-image">
+                                <div class="entry-media">
+                                    <img src="{{ $item->article_img }}" alt>
+                                </div>
+                                <div class="entry-meta">
+                                    <ul>
+                                        <li><i class="fi flaticon-user"></i> By <a href="#">Admin</a> </li>
+                                        <li><i class="ti-eye"></i> viewers
+                                            {{ $item->visitors }} </li>
+                                        <li><i class="fi flaticon-calendar"></i>
+                                            {{ $item->created_at->translatedFormat('j, F Y') }}</li>
+                                    </ul>
+                                </div>
+                                <div class="entry-details">
+                                    <h3><a href="#">{{ $item->article_title }}</a>
+                                    </h3>
+                                    <p>{{ Str::limit(strip_tags($item->article_detail), 200) }}</p>
+                                    <a href="#" class="read-more">READ MORE...</a>
+                                </div>
                             </div>
-                            <div class="entry-meta">
-                                <ul>
-                                    <li><i class="fi flaticon-user"></i> By <a href="#">Jenny Watson</a> </li>
-                                    <li><i class="fi flaticon-comment-white-oval-bubble"></i> Comments 35 </li>
-                                    <li><i class="fi flaticon-calendar"></i> 24 Jun 2021</li>
-                                </ul>
-                            </div>
-                            <div class="entry-details">
-                                <h3><a href="blog-single.html">8 Mistakes Founders Make When Starting a Business.</a></h3>
-                                <p>Consulting is a great career path if you want to build a broad skill set that includes
-                                    everything from critical thinking and strategic planning to communications. If you love
-                                    rising to a challenge.</p>
-                                <a href="blog-single.html" class="read-more">READ MORE...</a>
-                            </div>
-                        </div>
-                        <div class="post format-standard-image">
-                            <div class="entry-media">
-                                <img src="https://wpocean.com/html/tf/consultar-live/assets/images/blog/img-4.jpg" alt>
-                            </div>
-                            <div class="entry-meta">
-                                <ul>
-                                    <li><i class="fi flaticon-user"></i> By <a href="#">Jenny Watson</a> </li>
-                                    <li><i class="fi flaticon-comment-white-oval-bubble"></i> Comments 35 </li>
-                                    <li><i class="fi flaticon-calendar"></i> 24 Jun 2021</li>
-                                </ul>
-                            </div>
-                            <div class="entry-details">
-                                <h3><a href="blog-single.html">8 Mistakes Founders Make When Starting a Business.</a></h3>
-                                <p>Consulting is a great career path if you want to build a broad skill set that includes
-                                    everything from critical thinking and strategic planning to communications. If you love
-                                    rising to a challenge.</p>
-                                <a href="blog-single.html" class="read-more">READ MORE...</a>
-                            </div>
-                        </div>
+                        @endforeach
+
                         <div class="pagination-wrapper pagination-wrapper-left">
                             <ul class="pg-pagination">
                                 <li>
