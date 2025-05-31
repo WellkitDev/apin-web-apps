76ce5c70dc8e35c702ee3f30925998733df6d0b7
Wellkit <rahmatpurnomo808@gmail.com>
2025-06-01 00:04:29 +0700

    Refactor HTML structure in app layout for improved readability and consistency

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="">
-	<meta name="author" content="">
-	<meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
 
-  <title>Panel Admin - Nawalaedu.com</title>
+    <title>Panel Admin - Nawalaedu.com</title>
 
-  <!-- Fonts -->
+    <!-- Fonts -->
 
-  <!-- End fonts -->
-  
-  <!-- CSRF Token -->
-  <meta name="_token" content="{{ csrf_token() }}">
-  
-  <link rel="shortcut icon" href="{{ asset('assets/images/logo-nawala-vsm.png') }}">
+    <!-- End fonts -->
 
-  <!-- plugin css -->
-  
-  <!-- end plugin css -->
+    <!-- CSRF Token -->
+    <meta name="_token" content="{{ csrf_token() }}">
 
-  @stack('plugin-styles')
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo-nawala-vsm.png') }}">
 
-  <!-- common css -->
+    <!-- plugin css -->
+
+    <!-- end plugin css -->
+
+    @stack('plugin-styles')
+
+    <!-- common css -->
     <!-- Vendor css -->
     <link href="{{ asset('backend/assets/css/vendor.min.css') }}" rel="stylesheet" type="text/css" />
 
@@ -38,29 +39,40 @@
 
     <!-- Theme Config Js -->
     <script src="{{ asset('backend/assets/js/config.js') }}"></script>
-  <!-- end common css -->
+    <!-- end common css -->
 
-  @stack('style')
+    @stack('style')
 </head>
-<body data-base-url="{{url('/')}}">
 
-  <div class="wrapper" id="app">
-    @include('backend.layouts.side-nav-menu')
-    @include('backend.layouts.app-topbar')
-    <div class="page-content">
-      @include('backend.layouts.message')
-        @yield('content')
-        @include('backend.layouts.footer')
+<body data-base-url="{{ url('/') }}">
+
+    <div class="wrapper" id="app">
+        @include('backend.layouts.side-nav-menu')
+        @include('backend.layouts.app-topbar')
+        <div class="page-content">
+            @include('backend.layouts.message')
+            @yield('content')
+            @include('backend.layouts.footer')
+        </div>
+        <!-- END page-content -->
     </div>
-    <!-- END page-content -->
-  </div>
-  <!-- END wrapper -->
-  @include('backend.layouts.themes')
+    <!-- END wrapper -->
+    @include('backend.layouts.themes')
 
     <!-- base js -->
     <script src="{{ asset('backend/assets/js/vendor.min.js') }}"></script>
     <script src="{{ asset('backend/assets/js/app.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@2.8.2/dist/alpine.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script> --}}
 
     <!-- end base js -->
 
@@ -74,4 +86,5 @@
 
     @stack('custom-scripts')
 </body>
+
 </html>
