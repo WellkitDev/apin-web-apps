689d106e9d7cf642adaf55566fa6dfb7a0d6236d
Wellkit <rahmatpurnomo808@gmail.com>
2025-06-01 00:03:16 +0700

    Create migration for books table with necessary fields

---
+++ database/migrations/2025_05_29_155949_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('cover');
+            $table->string('pdf');
+            $table->string('isbn')->nullable();
+            $table->string('link_isbn')->nullable();
+            $table->string('link_google_books')->nullable();
+            $table->string('halaman')->nullable();
+            $table->string('author')->nullable();
+            $table->string('description');
+            $table->string('publish_date')->nullable();
+            $table->string('amout')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
