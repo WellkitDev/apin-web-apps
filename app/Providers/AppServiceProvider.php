8a044cd3a88c19a2a607c9a2ca7b45d4e82743a5
Wellkit <rahmatpurnomo808@gmail.com>
2025-06-01 23:31:10 +0700

    Enable Bootstrap styling for pagination in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Page;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Paginator::useBootstrap();
         $menuPages = Page::where('is_active', true)->orderBy('type', 'asc')->get()->groupBy('type');
 
         View::share('menuPages', $menuPages);
