75588ac1ef40633689e67613137c7826ed49aced
Wellkit <rahmatpurnomo808@gmail.com>
2025-06-01 23:17:32 +0700

    Enhance book display layout and add description truncation with detail link

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -20,26 +20,28 @@
     <!-- end of wpo-breadcumb-section-->
     <section class="wpo-blog-pg-section section-padding">
         <div class="container">
-            @foreach ($books as $item)
-                <div class="row">
+            <div class="row">
+                @foreach ($books as $item)
                     <div class="col-6">
-                        <div class="wpo-blog-pg-wrap association">
-                            <table class="table table-responsive table-striped">
+                        <div class="wpo-blog-pg-wrap association wow fadeInUp">
+                            <table class="table table-responsive table-striped ">
                                 <tbody>
                                     <tr>
-                                        <td>
-                                            <div class="text-center">
-                                                <a href="">
-                                                    <img src="{{ asset($item->cover) }}" alt="" width="200px">
-                                                </a>
-                                            </div>
+                                        <td style="padding: 15px" width="150">
+                                            <a href="">
+                                                <img src="{{ asset($item->cover) }}" alt="" width="200px">
+                                            </a>
                                         </td>
-                                        <td>
-                                            <div class="col-md-10">
+                                        <td style="padding: 15px">
+                                            <div class="col-md-10 text-left">
                                                 <h4><a href="">{{ $item->title }}</a></h4>
-                                                <p>Pendidikan</p>
-                                                <p>Pendidikan</p>
-                                                <p><a href="#">Link url kampus</a></p>
+                                                <p>ISBN: <b>{{ $item->isbn != null ? $item->isbn : '-' }}</b></p>
+                                                <p>Deskripsi:</p>
+                                                <p>
+                                                    {{ Str::limit(strip_tags($item->description), 150) }}
+                                                </p>
+                                                <p><a href="{{ route('buku.detail', ['slug' => $item->slug]) }}">Detail</a>
+                                                </p>
                                             </div>
                                         </td>
                                     </tr>
@@ -47,8 +49,8 @@
                             </table>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
 
         </div>
     </section>
